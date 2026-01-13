86ba11eeef769bba461938dad85f3a748b71fb9b
Chunpo Wang <1505070+artoowang@users.noreply.github.com>
2026-01-13 13:31:14 +0800

    Added initial Glasses dependencies and activity

--- glasses/build.gradle.kts
+++ glasses/build.gradle.kts
@@ -1,18 +1,14 @@
 plugins {
     alias(libs.plugins.android.library)
+    // This is needed to run AndroidCommonConventionPlugin.kt to configure
+    // the project similar to other modules. E.g., Java version.
+    alias(libs.plugins.homeassistant.android.common)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "io.homeassistant.companion.android.glasses"
-    compileSdk {
-        version = release(36)
-    }
-
     defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -25,20 +21,14 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    // TDOO: This is said to be deprecated. Do we still need it?
-    // kotlinOptions {
-    //     jvmTarget = "11"
-    // }
 }
 
 dependencies {
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation("androidx.xr.glimmer:glimmer:1.0.0-alpha03")
+    implementation("androidx.xr.projected:projected:1.0.0-alpha03")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ glasses/src/main/kotlin/io/homeassistant/companion/android/glasses/GlassesActivity.kt
@@ -0,0 +1,10 @@
+package io.homeassistant.companion.android.glasses
+
+import android.app.Activity
+import android.os.Bundle
+
+class GlassesActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
