34e7ceec7b5a7ba3ec5a710f2913609a18668104
Chunpo Wang <1505070+artoowang@users.noreply.github.com>
2026-01-13 15:48:53 +0800

    Added skeleton GlassesActivity implementation

--- glasses/src/main/kotlin/io/homeassistant/companion/android/glasses/GlassesActivity.kt
+++ glasses/src/main/kotlin/io/homeassistant/companion/android/glasses/GlassesActivity.kt
@@ -1,13 +1,76 @@
 package io.homeassistant.companion.android.glasses
 
-import android.app.Activity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.content.ContextCompat
+import androidx.xr.projected.experimental.ExperimentalProjectedApi
+import androidx.xr.projected.permissions.ProjectedPermissionsRequestParams
+import androidx.xr.projected.permissions.ProjectedPermissionsResultContract
 import timber.log.Timber
 
-class GlassesActivity : Activity() {
+// This is modified from AI Sample Catalog, Gemini Live Todo example.
+class GlassesActivity : ComponentActivity() {
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Permission Utilities.
+
+    // Keeps track if the required permissions by glasses are granted.
+    private var isPermissionsGranted by mutableStateOf(false)
+    private val requiredPermissions = listOf(
+        Manifest.permission.RECORD_AUDIO
+    )
+
+    @OptIn(ExperimentalProjectedApi::class)
+    private val requestPermissionLauncher: ActivityResultLauncher<List<ProjectedPermissionsRequestParams>> =
+        registerForActivityResult(ProjectedPermissionsResultContract()) { results ->
+            val granted = requiredPermissions.all { permission ->
+                results[permission] == true
+            }
+            isPermissionsGranted = granted
+            setupContent()
+        }
+
+    private fun checkAllPermissionsGranted(): Boolean {
+        return requiredPermissions.all { permission ->
+            ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    @OptIn(ExperimentalProjectedApi::class)
+    private fun requestPermissions() {
+        requestPermissionLauncher.launch(
+            listOf(
+                ProjectedPermissionsRequestParams(
+                    permissions = requiredPermissions,
+                    rationale = "We need microphone access to continue to the main experience."
+                )
+            )
+        )
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+    private fun setupContent() {
+        // TODO
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         Timber.d("ZZZ: GlassesActivity.onCreate: savedInstanceState=$savedInstanceState")
+
+        val allGranted = checkAllPermissionsGranted()
+        isPermissionsGranted = allGranted
+
+        setupContent()
+
+        if (!allGranted) {
+            requestPermissions()
+        }
     }
 }
