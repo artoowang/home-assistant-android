a9eb7e6aa329c1a347880ade7cb9580b84aeddb3
Chunpo Wang <1505070+artoowang@users.noreply.github.com>
2026-01-13 10:47:10 +0800

    Added glasses module
    - File > New > New Module
    - Package name: io.homeassistant.companion.android.glasses
    - Commented out kotlinOptions that Android Studio complains
    
    Confirmed it still builds and runs.

---
+++ glasses/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "io.homeassistant.companion.android.glasses"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    // TDOO: This is said to be deprecated. Do we still need it?
+    // kotlinOptions {
+    //     jvmTarget = "11"
+    // }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
