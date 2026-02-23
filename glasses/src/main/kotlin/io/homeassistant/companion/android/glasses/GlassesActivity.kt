e2ccd6ae4f739109d7a8b15a2e505a71754423f3
Chunpo Wang <1505070+artoowang@users.noreply.github.com>
2026-02-22 21:10:51 -0800

    WIP: Try adding takePhoto

--- glasses/src/main/kotlin/io/homeassistant/companion/android/glasses/GlassesActivity.kt
+++ glasses/src/main/kotlin/io/homeassistant/companion/android/glasses/GlassesActivity.kt
@@ -1,14 +1,18 @@
 package io.homeassistant.companion.android.glasses
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CaptureRequest
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Range
 import android.util.Size
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
@@ -17,6 +21,7 @@ import androidx.camera.camera2.interop.CaptureRequestOptions
 import androidx.camera.camera2.interop.ExperimentalCamera2Interop
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.resolutionselector.ResolutionSelector
 import androidx.camera.core.resolutionselector.ResolutionStrategy
 import androidx.camera.lifecycle.ProcessCameraProvider
@@ -137,12 +142,108 @@ class GlassesActivity : ComponentActivity() {
         }
     }
 
+    @androidx.annotation.OptIn(ExperimentalCamera2Interop::class)
     private suspend fun setUpCamera() {
         Timber.d("ZZZ: setUpCamera")
 
         val cameraProvider = ProcessCameraProvider.getInstance(this).await()
         Timber.d("ZZZ: cameraProvider=$cameraProvider")
         Timber.d("ZZZ: cameraProvider.availableCameraInfos=${cameraProvider.availableCameraInfos}")
+
+        // Is "back" the primary glasses camera?
+        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+        val cameraInfo = cameraProvider.getCameraInfo(cameraSelector)
+        val camera2CameraInfo = Camera2CameraInfo.from(cameraInfo)
+        val cameraCharacteristics =
+            camera2CameraInfo.getCameraCharacteristic(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+        Timber.d("ZZZ: cameraCharacteristics=$cameraCharacteristics")
+
+        val targetResolution = Size(1920, 1080)
+        val resolutionStrategy = ResolutionStrategy(
+            targetResolution,
+            ResolutionStrategy.FALLBACK_RULE_CLOSEST_LOWER,
+        )
+
+        val resolutionSelector = ResolutionSelector.Builder()
+            .setResolutionStrategy(resolutionStrategy)
+            .build()
+
+        // Initialize the ImageCapture use case.
+        val imageCapture = ImageCapture.Builder()
+            // Optional: Configure resolution, format, etc.
+            .setResolutionSelector(resolutionSelector)
+            .build()
+
+        try {
+            // Unbind use cases before rebinding
+            cameraProvider.unbindAll()
+
+            // 4. Bind use cases to camera
+            cameraProvider.bindToLifecycle(
+                this as LifecycleOwner,
+                cameraSelector,
+                imageCapture,
+            )
+
+            Timber.d("ZZZ: imageCapture=$imageCapture")
+            takePhoto(this, imageCapture)
+        } catch (exc: Exception) {
+            // This catches exceptions like IllegalStateException if use case binding fails
+            Timber.e(exc, "Use case binding failed")
+        }
+    }
+
+    // TODO
+    private fun createOutputFileOptions(context: Context): ImageCapture.OutputFileOptions {
+        return ImageCapture.OutputFileOptions.Builder(
+            context.contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, "IMG_${System.currentTimeMillis()}")
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/MyApp")
+            },
+        ).build()
+    }
+
+    // TODO
+    private fun takePhoto(context: Context, imageCapture: ImageCapture) {
+        Timber.d("ZZZ: takePhoto")
+
+        val outputOptions = createOutputFileOptions(context)
+
+        imageCapture.takePicture(
+            /* outputFileOptions = */
+            outputOptions,
+            /* executor = */
+            ContextCompat.getMainExecutor(context),
+            /* imageSavedCallback = */
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onCaptureStarted() {
+                    Timber.d("ZZZ: onCaptureStarted")
+                }
+
+                override fun onCaptureProcessProgressed(progress: Int) {
+                    Timber.d("ZZZ: onCaptureProcessProgressed: $progress")
+                }
+
+                override fun onPostviewBitmapAvailable(bitmap: Bitmap) {
+                    Timber.d("ZZZ: onPostviewBitmapAvailable: $bitmap")
+                }
+
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = outputFileResults.savedUri
+                    Timber.d("ZZZ: Photo saved to gallery: $savedUri")
+                    Toast.makeText(context, "Photo saved!", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Timber.e(exception, "ZZZ: Photo capture failed: ${exception.message}")
+                    Toast.makeText(context, "Failed to capture photo", Toast.LENGTH_SHORT).show()
+                }
+            },
+        )
     }
 
     override fun onPause() {
@@ -192,8 +293,6 @@ class GlassesActivity : ComponentActivity() {
                 // Get supported streaming resolutions.
                 val cameraInfo = cameraProvider.getCameraInfo(cameraSelector)
                 val camera2CameraInfo = Camera2CameraInfo.from(cameraInfo)
-                val cameraCharacteristics =
-                    camera2CameraInfo.getCameraCharacteristic(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
 
                 // Define the resolution strategy.
                 val targetResolution = Size(1920, 1080)
@@ -204,12 +303,6 @@ class GlassesActivity : ComponentActivity() {
 
                 val resolutionSelector = ResolutionSelector.Builder()
                     .setResolutionStrategy(resolutionStrategy)
-                    .build()
-
-                // If you have other continuous use cases bound, such as Preview or ImageAnalysis, you can use  Camera2 Interop's CaptureRequestOptions to set the FPS
-                val fpsRange = Range(30, 30)
-                val captureRequestOptions = CaptureRequestOptions.Builder()
-                    .setCaptureRequestOption(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, fpsRange)
                     .build()
 
                 // Initialize the ImageCapture use case.
